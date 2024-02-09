4b4ac99f5e6d20df70c97e8623fe11715acb6014
JuanUNAL <159583968+JuanUNAL@users.noreply.github.com>
2024-02-09 17:02:49 -0500

    Update juanma.py

--- juanma.py
+++ juanma.py
@@ -1,10 +1,17 @@
 # import webbrowser
+# from IPython.display import clear_output
+
+# def limpiar():
+#     clear_output(wait=True)
 
 def ejecutar(n):
   print("Hola", n)
 
+def sumar(a, b):
+  return a + b
+
 # def editarCodigo():
 #   webbrowser.open_new_tab("https://github.com/JuanUNAL/code/edit/main/juanma.py")
 
-def limpiar(n):
-  print("\b" * 1000)
+# def limpiar(n):
+#   print("\b" * 1000)
